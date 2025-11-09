c2cfecd3acc804fd2f7aaaa8eab11fbfebdbba37
annaPP07 <annapp26.09@gmail.com>
2025-11-09 23:40:05 +0300

    Update java.java

--- лр3/java.java
+++ лр3/java.java
@@ -1,1 +1,47 @@
+1. Бинарная куча
+import java.util.PriorityQueue;
+PriorityQueue<Integer> heap = new PriorityQueue<>();
+heap.add(10);
+heap.add(5);
+int min_val = heap.poll();  // 5
 
+2. Биномиальная куча
+class BinomialNode {
+    int key;
+    List<BinomialNode> children;
+    BinomialNode parent;
+    public BinomialNode(int key) {
+        this.key = key;
+        this.children = new ArrayList<>();
+        this.parent = null;
+    }
+}
+class BinomialHeap {
+    List<BinomialNode> trees = new ArrayList<>();
+}
+
+3. Куча Фибоначчи
+class FibNode {
+    int key;
+    int degree;
+    FibNode parent, child, next, prev;
+    boolean mark;
+    public FibNode(int key) {
+        this.key = key;
+        this.degree = 0;
+        this.mark = false;
+        this.parent = this.child = null;
+        this.next = this.prev = this;
+    }
+}
+class FibonacciHeap {
+    FibNode minNode;
+    int totalNodes;
+}
+
+4. Хэш‑таблица 
+import java.util.HashMap;
+HashMap<String, String> hashTable = new HashMap<>();
+hashTable.put("key1", "value1");
+String value = hashTable.get("key1");
+hashTable.remove("key1");
